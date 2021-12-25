85a265706b5e8cdcd097aafb30a3f51ca85a0f3d
David Gustafsson <davidgust99@gmail.com>
2021-12-25 15:15:38 +0100

    Switched debugger to nodemon

--- app.js
+++ app.js
@@ -10,15 +10,7 @@ app.get('/', (req, res) => {
   res.send(watches);
 });
 
-let newWatch = {
-  id: 4,
-  link: 'https://www.link4.com',
-  name: 'name 5',
-  'last scraped': '9-10-2021',
-  isActive: true,
-};
-
-app.post('/test', function (req, res) {
+app.post('/write', function (req, res) {
   var obj = {
     table: [],
   };
@@ -44,37 +36,10 @@ app.post('/test', function (req, res) {
   });
 });
 
-app.get('/write', (req, res) => {
-  var obj = {
-    table: [],
-  };
-  var json = JSON.stringify(obj);
-  fs.readFile('data.json', 'utf8', function readFileCallback(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      obj = JSON.parse(data); //now it an object
-      let newWatch = {
-        id: 4,
-        link: 'https://www.link4.com',
-        name: 'name 4',
-        'last scraped': '4',
-        isActive: true,
-      };
-
-      obj.watches.push(newWatch); //add some data
-      json = JSON.stringify(obj, null, 2); //convert it back to json
-      fs.writeFile('data.json', json, function (err) {
-        // log error
-        if (err) console.log('error', err);
-        else {
-          let rawdata = fs.readFileSync('data.json'); // Show data to user
-          let watches = JSON.parse(rawdata);
-          res.send(watches);
-        }
-      });
-    }
-  });
+app.get('/', (req, res) => {
+  let rawdata = fs.readFileSync('data.json'); // Show data to user
+  let watches = JSON.parse(rawdata);
+  res.send(watches);
 });
 
 app.listen(port, () => {
