e507b38c8a9d996237db3b70e882544c958da745
David Gustafsson <davidgust99@gmail.com>
2021-12-25 19:10:53 +0100

    Date & time, uuid and improved add-watch

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 const express = require('express');
-const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.delete('/:link', function (req, res) {
-  const id = crypto.randomBytes(3 * 4).toString('base64'); // För att skapa en GUID
-  console.log(id); // hLjmgBrIjGqiPTsf
+function getDateAndTime() {
+  let datetime = new Date().toLocaleString('sv-SE', {
+    timeZone: 'Europe/Stockholm',
+  });
+  return datetime;
+}
 
-  let removeWatch = req.params.link;
+app.delete('/:id', function (req, res) {
+  let watchToRemove = req.params.id;
   let data = fs.readFileSync('data.json');
   let json = JSON.parse(data);
   let watches = json.watches;
   json.watches = watches.filter((watch) => {
-    return watch.link !== removeWatch;
+    return watch.id !== watchToRemove;
   });
   fs.writeFileSync('data.json', JSON.stringify(json, null, 2));
-  res.send(`Watch with link: ${req.params.link} deleted.`);
+  res.send(`Watch with id: ${req.params.id} deleted.`);
 });
 
 app.post('/add-watch', function (req, res) {
-  var obj = {
-    table: [],
-  };
-  var json = JSON.stringify(obj);
+  var obj = {};
   fs.readFile('data.json', 'utf8', function readFileCallback(err, data) {
     if (err) {
       console.log(err);
     } else {
       obj = JSON.parse(data); //now it an object
-      obj.watches.push(req.body); //add some data
+
+      let newWatch = req.body;
+      newWatch['addedOn'] = getDateAndTime();
+      newWatch['id'] = uuidv4();
+      obj.watches.push(newWatch); //add some data
+
       json = JSON.stringify(obj, null, 2); //convert it back to json
       fs.writeFile('data.json', json, function (err) {
-        // log error
         if (err) console.log('error', err);
         else {
           let rawdata = fs.readFileSync('data.json');
@@ -47,7 +52,7 @@ app.post('/add-watch', function (req, res) {
 });
 
 app.get('/watches', (req, res) => {
-  let rawdata = fs.readFileSync('data.json'); // Show data to user
+  let rawdata = fs.readFileSync('data.json');
   let obj = JSON.parse(rawdata);
   res.send(obj);
 });
