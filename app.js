1f20e131a8c6ab2fbebbe865e08bc8c7cb8cce0d
David Gustafsson <davidgust99@gmail.com>
2021-12-25 23:14:51 +0100

    made separate routes file and implemented logging

--- app.js
+++ app.js
@@ -1,59 +1,12 @@
+'use strict';
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+
 const app = express();
+const routes = require('./routes');
+
 app.use(express.json());
+app.use(routes);
 const port = 3000;
-
-function getDateAndTime() {
-  let datetime = new Date().toLocaleString('sv-SE', {
-    timeZone: 'Europe/Stockholm',
-  });
-  return datetime;
-}
-
-app.delete('/:id', function (req, res) {
-  let watchToRemove = req.params.id;
-  let data = fs.readFileSync('data.json');
-  let json = JSON.parse(data);
-  let watches = json.watches;
-  json.watches = watches.filter((watch) => {
-    return watch.id !== watchToRemove;
-  });
-  fs.writeFileSync('data.json', JSON.stringify(json, null, 2));
-  res.status(200).send(`Watch with id: ${req.params.id} deleted.`);
-});
-
-app.post('/add-watch', function (req, res) {
-  var obj = {};
-  fs.readFile('data.json', 'utf8', function readFileCallback(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      obj = JSON.parse(data); //now it an object
-
-      let newWatch = req.body;
-      newWatch['addedOn'] = getDateAndTime();
-      newWatch['id'] = uuidv4();
-      obj.watches.push(newWatch); //add some data
-
-      json = JSON.stringify(obj, null, 2); //convert it back to json
-      fs.writeFile('data.json', json, function (err) {
-        if (err) console.log('error', err);
-        else {
-          //obj.watches[1]; // För att hämta för elementet
-          res.status(201).send(`Added watch with name: ${req.body.name}`); // Hämta alla klockor
-        }
-      });
-    }
-  });
-});
-
-app.get('/watches', (req, res) => {
-  let rawdata = fs.readFileSync('data.json');
-  let obj = JSON.parse(rawdata);
-  res.status(200).send(obj);
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const fs = require('fs');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, prettyPrint } = format;
+const { v4: uuidv4 } = require('uuid');
+
+const logger = createLogger({
+  format: combine(timestamp(), prettyPrint()),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'ks_web_scraper.log' }),
+  ],
+});
+
+function getDateAndTime() {
+  let datetime = new Date().toLocaleString('sv-SE', {
+    timeZone: 'Europe/Stockholm',
+  });
+  return datetime;
+}
+
+router.delete('/:id', function (req, res) {
+  let watchToRemove = req.params.id;
+  let data = fs.readFileSync('data.json');
+  let json = JSON.parse(data);
+  let watches = json.watches;
+  json.watches = watches.filter((watch) => {
+    return watch.id !== watchToRemove;
+  });
+  fs.writeFile('data.json', JSON.stringify(json, null, 2), (err) => {
+    if (err) {
+      logger.log({
+        level: 'error',
+        message: `Could not delete in route: delete/:id. Error message ${err.message}`,
+      });
+    } else {
+      res.status(200).send(`Watch with id: ${req.params.id} deleted.`);
+    }
+  });
+});
+
+router.post('/add-watch', function (req, res) {
+  let obj = {};
+  fs.readFile('data.json', 'utf8', function readFileCallback(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      obj = JSON.parse(data); //now it an object
+
+      let newWatch = req.body;
+      newWatch['addedOn'] = getDateAndTime();
+      newWatch['id'] = uuidv4();
+      obj.watches.push(newWatch); //add some data
+      logger.log({
+        level: 'error',
+        message: `Logger test`,
+      });
+
+      let json = JSON.stringify(obj, null, 2); //convert it back to json
+      fs.writeFile('data.json', json, function (err) {
+        if (err) {
+          logger.log({
+            level: 'error',
+            message: `Could not write to data.json in route: add-watches. Error message ${err.message}`,
+          });
+        } else {
+          //obj.watches[1]; // För att hämta för elementet
+          res.status(201).send(`Added watch with name: ${req.body.name}`); // Hämta alla klockor
+        }
+      });
+    }
+  });
+});
+
+router.get('/watches', (req, res) => {
+  let rawdata = fs.readFileSync('data.json');
+  let obj = JSON.parse(rawdata);
+  res.status(200).send(obj);
+});
+
+module.exports = router;
