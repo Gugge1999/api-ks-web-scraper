b112c92c02d3c8fdd531f7686204b99ba4de531d
David Gustafsson <davidgust99@gmail.com>
2022-01-23 14:52:18 +0100

    Added note about logging with winston

--- services/LoggerService.js
+++ services/LoggerService.js
@@ -1,6 +1,8 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, prettyPrint } = format;
 
+// Se hur winston-logging hanteras här: https://github.com/galenmaly/lighterpack/blob/master/server/log.js
+
 const logger = createLogger({
   format: combine(timestamp(), prettyPrint()),
   transports: [
