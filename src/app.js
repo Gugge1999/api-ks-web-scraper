61ede46c199bbd89ec76b62b50e1c6a35a8172ac
David G <davidgust99@gmail.com>
2022-02-13 17:15:05 +0100

    Code clean up

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const routes = require('./routes/routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Add headers before the routes are defined
+app.use(cors()); // Add cors before the routes are defined
 app.use(routes);
 const port = 3000;
 
-scraper.run();
+//scraper.run();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
