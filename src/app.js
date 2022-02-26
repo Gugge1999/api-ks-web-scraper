042450096586a721873d4de30beaae290e170b42
David G <davidgust99@gmail.com>
2022-02-26 22:36:17 +0100

    Delete unused packages

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ import { scrapeAllWatches } from './services/scraper.service.js';
 
 const app = express();
 app.use(json());
-app.use(cors()); // Add cors before the routes are defined
+app.use(cors()); // Lägg till cors FÖRE routes
 app.use(routes);
 const port = process.env.PORT || 3000;
+
+// Relativ sökväg till Angular dist mappen. Startar från "C:\Code\api\ks_web_scraper_api"
+// app.use(
+//   '/',
+//   express.static('../../angular_projects/ks-web-scraper/dist/ks-web-scraper')
+// );
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
