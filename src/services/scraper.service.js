17a1d5f7461df3526a6a363bbc29e8424b65a712
David G <davidgust99@gmail.com>
2022-05-22 18:17:18 +0200

    First commit on comparing multiple scraped watches

--- src/services/scraper.service.js
+++ src/services/scraper.service.js
@@ -11,39 +11,60 @@ import { updateStoredWatch, getAllWatches } from './db.service.js';
 import { errorLogger, infoLogger } from './logger.service.js';
 
 export async function scrapeWatchInfo(link) {
-  const watchInfo = {
-    watchName: '',
-    postedDate: '',
-    watchLink: ''
-  };
-
   const response = await fetch(link);
   const body = await response.text();
 
   const $ = cheerio.load(body);
 
-  watchInfo.watchName = $('.contentRow-title')
-    .children()
-    .first()
-    .text()
-    .replace(/Tillbakadragen|Avslutad|Säljes|OHPF|Bytes|\//gi, '')
-    .trim();
+  const scrapedWatchArr = [];
 
-  if (watchInfo.watchName === '') {
-    errorLogger.error({ message: 'Watch name not found' });
-    throw new Error();
+  const allContentRowTitle = $('.contentRow-title').children();
+
+  for (let i = 0; i < allContentRowTitle.length; i += 1) {
+    const currentWatchInfo = {
+      watchName: '',
+      postedDate: '',
+      watchLink: ''
+    };
+
+    const titleArr = allContentRowTitle[i].children;
+    const lastIndex = Object.keys(titleArr).length - 1;
+
+    let watchName = '';
+
+    // Om det lastIndex === 3 vet vi att css klassen textHighlight finns.
+    // Hämta i så fall sista och näst sista.
+    if (lastIndex === 3) {
+      const watchTitleLastChildText =
+        allContentRowTitle[i].children[lastIndex].data;
+
+      // Text från textHighlight
+      watchName =
+        allContentRowTitle[i].children[lastIndex - 1].children[0].data +
+        watchTitleLastChildText;
+    } else {
+      watchName = $('.contentRow-title')
+        .children()
+        [i].children[lastIndex].data.trim();
+    }
+
+    if (watchName === '') {
+      errorLogger.error({ message: 'Watch name not found.' });
+      throw new Error();
+    }
+
+    // attr "datetime" finns också. Format: 2022-02-24T18:12:49+0100
+    const watchDate = $('.u-dt')[i].attribs.title; // Format: 22 Maj 2022 kl 14:16
+
+    const watchLink = `https://klocksnack.se${allContentRowTitle[i].attribs.href}`;
+
+    currentWatchInfo.watchName = watchName;
+    currentWatchInfo.postedDate = watchDate;
+    currentWatchInfo.watchLink = watchLink;
+    scrapedWatchArr.push(currentWatchInfo);
   }
 
-  // Format: 24 Februari 2022 kl 18:12
-  // attr "datetime" finns också. Format: 2022-02-24T18:12:49+0100
-  watchInfo.postedDate = $('.u-dt').attr('title');
-
-  watchInfo.watchLink = `https://klocksnack.se${$('.contentRow-title')
-    .children()
-    .first()
-    .attr('href')}`;
-
-  return watchInfo;
+  return scrapedWatchArr;
 }
 
 export async function scrapeAllWatches() {
