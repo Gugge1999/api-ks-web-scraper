e1de5e859761a1f6688b8b5b753c8c936d817aaa
David G <davidgust99@gmail.com>
2022-07-02 14:37:48 +0200

    Add database config for dev and prod

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ import { Watch } from './entity/Watch.js';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const devConfig = {
   type: 'postgres',
   host: process.env.PGHOST,
   port: process.env.PGPORT,
@@ -19,4 +19,24 @@ export const AppDataSource = new DataSource({
   entities: [Watch],
   migrations: [],
   subscribers: []
-});
+};
+
+const prodConfig = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: false,
+  entities: [Watch],
+  migrations: [],
+  subscribers: [],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+};
+
+export const AppDataSource = new DataSource(
+  process.env.NODE_ENV === 'develop' ? devConfig : prodConfig
+);
