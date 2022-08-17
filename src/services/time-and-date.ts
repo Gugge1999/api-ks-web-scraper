ea8b11f732cbba98f2cc0af19bdbf2fd94034330
David G <davidgust99@gmail.com>
2022-08-17 18:35:54 +0200

    Add function params comments to dates and times

--- src/services/time-and-date.ts
+++ src/services/time-and-date.ts
@@ -2,17 +2,30 @@ import { formatInTimeZone } from 'date-fns-tz/esm';
 
 const timeZone = 'Europe/Stockholm';
 
+/**
+ * @return k:mm:ss.
+ */
 export function currentTime() {
   return formatInTimeZone(new Date(), timeZone, 'k:mm:ss');
 }
 
+/**
+ * @return yyyy-MM-dd k:mm:ss.
+ */
 export function dateAndTime() {
   return formatInTimeZone(new Date(), timeZone, 'yyyy-MM-dd k:mm:ss');
 }
 
+/**
+ * @return k:mm:ss yyyy-MM-dd.
+ */
 export function dateAndTimeWithTimeFirst() {
   return formatInTimeZone(new Date(), timeZone, 'k:mm:ss yyyy-MM-dd');
 }
+
+/**
+ * @return yyyy-MM-dd.
+ */
 export function todaysDate() {
   return formatInTimeZone(new Date(), timeZone, '', 'yyyy-MM-dd');
 }
