ab251b316f45985ca3d395c04967ef9e66ad0628
David G <davidgust99@gmail.com>
2024-04-13 20:04:02 +0200

    Remove express

--- src/services/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-// eslint-disable-next-line no-unused-vars
-export function errorHandler(error: string, req: Request, res: Response, next: NextFunction) {
-  res.status(500).json(error);
-}
